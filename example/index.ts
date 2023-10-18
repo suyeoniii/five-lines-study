acfbb81b754c93f6c945d6c53f72a283374adc1f
suyeoniii <ssy4230@naver.com>
2023-10-18 22:58:12 +0900

    3.26: handleInput 메서드 추출

--- example/index.ts
+++ example/index.ts
@@ -128,11 +128,15 @@ function updateTitle(x: number, y: number) {
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT) moveHorizontal(-1);
-    else if (current === Input.RIGHT) moveHorizontal(1);
-    else if (current === Input.UP) moveVertical(-1);
-    else if (current === Input.DOWN) moveVertical(1);
+    handleInput(current);
   }
+}
+
+function handleInput(current: Input) {
+  if (current === Input.LEFT) moveHorizontal(-1);
+  else if (current === Input.RIGHT) moveHorizontal(1);
+  else if (current === Input.UP) moveVertical(-1);
+  else if (current === Input.DOWN) moveVertical(1);
 }
 
 function draw() {
